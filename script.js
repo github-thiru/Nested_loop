f20d221959f1882c0f920e881eab1e934d646d10
github-thiru <thiruthorlikonda967@gmail.com>
2024-11-16 22:05:03 +0530

    push

---
+++ script.js
@@ -0,0 +1,28 @@
+///for loop t 1to 5 table print
+num=5
+for(let i=1;i<=5;i++){
+    for(j=1;j<=10;j++){
+        console.log(`${i}x${j}=${i*j}`)
+    }
+}
+
+
+///while loop
+let i = -10;
+
+while (i <= 1) { 
+    console.log(i); 
+    i++; 
+}
+
+
+///do while
+let n=10
+i=1
+do{
+    console.log(i)
+i++
+}while(i<=n)
+
+
+
